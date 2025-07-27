e8d2a676c6db5f9af6f91677d6628e5400e900f0
esketids <129664098+esketids@users.noreply.github.com>
2025-07-27 18:55:15 +0800

    Update default.sh

--- default.sh
+++ default.sh
@@ -16,9 +16,10 @@ PIP_PACKAGES=(
 )
 
 NODES=(
-    #"https://github.com/ltdrdata/ComfyUI-Manager"
-    #"https://github.com/cubiq/ComfyUI_essentials"
+    "https://github.com/ltdrdata/ComfyUI-Manager"
+    "https://github.com/cubiq/ComfyUI_essentials"
     "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
+    "https://github.com/LevelPixel/ComfyUI-LevelPixel"
     
     
 )
